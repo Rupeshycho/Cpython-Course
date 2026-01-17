493c2eee7cb8eb5a3e4949909ca5f197022bfef5
Rupeshycho <crrupesh0@gmail.com>
2026-01-17 22:52:00 +0545

    variable and data types added

---
+++ day6.py
@@ -0,0 +1,31 @@
+a=2313241324124123412341
+print(a)
+print("Type of a is: ",type(a))
+b="Harry"
+print("Type of b: ",type(b))
+c=True
+print("Type of C:",type(c))
+
+d=None
+print("Type of None is: ",type(d))
+
+cmplx_number=complex(8,2)
+print("Type of complex Number: ",type(cmplx_number))
+
+a=input("Enter First Name: \n")
+b=input("Enter the Second Name: \n")
+print(f'Full Name: {a+b}')
+
+list1=[8,2,3,[-4,5],["apple","banana"]]
+print(list1)
+
+tup1=(8,2,3,[-4,5],["apple","banana"])  #immutable
+print(f'Tuple: {tup1}')
+
+dict1={"Name":"RupeshAnju","Age:":20,"canVote":True}
+print(f'Dictionary: {dict1}')
+
+
+
+
+
