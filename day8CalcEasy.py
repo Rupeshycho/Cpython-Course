798bd45bb2855893ae2806291eab16f61f8aaf57
Rupeshycho <crrupesh0@gmail.com>
2026-01-19 23:56:00 +0545

    choice added

--- day8CalcEasy.py
+++ day8CalcEasy.py
@@ -1,21 +1,28 @@
-num1=float(input("Enter the first number: "))
-operator=input("Enter the Operator(+,-,*,/): ")
-num2= float(input("Enter the second number: "))
+while True:
+    num1=float(input("Enter the first number: "))
+    operator=input("Enter the Operator(+,-,*,/): ")
+    num2= float(input("Enter the second number: "))
 
-if operator=='+':
-    print("Result: ", num1+num2)
+    if operator=='+':
+        print("Result: ", num1+num2)
 
-elif operator=='-':
-    print(f'Result: {num1-num2}')
+    elif operator=='-':
+        print(f'Result: {num1-num2}')
 
-elif operator=='*':
-    print(f'Result: {num1*num2}')
-    
-elif operator=='/':
-    if num2 ==0:
-        print("ZeroDivisionError: Cannot divide by Zero")
-    else: 
-        print("Result: ",num1/num2)
-else:
-    print("Invalid Operation")
-                          
+    elif operator=='*':
+        print(f'Result: {num1*num2}')
+        
+    elif operator=='/':
+        if num2 ==0:
+            print("ZeroDivisionError: Cannot divide by Zero")
+        else: 
+            print("Result: ",num1/num2)
+    else:
+        print("Invalid Operation")
+                
+    choice=input("Do you want to exit?(y/n)").lower()
+    if choice=='y':
+        print("\n Thank you\nExiting.....")
+        break        
+
+
