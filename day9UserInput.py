a47567d248ca19086b5abc386ac0a28c622ada10
Rupeshycho <crrupesh0@gmail.com>
2026-01-25 01:37:21 +0545

    general user input added

---
+++ day9UserInput.py
@@ -0,0 +1,11 @@
+a= input("Enter your name: ")
+print("Hello, ",a,"!\Welcome sir 🙏")
+
+x=input("Enter first number: ")
+y=input("Enter second number: ")
+print(f'x+y= {x+y} (no addition cause its in string , USE: int(input("...")))')
+
+
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+print("Quotient: ",a//b,"\n(without any remainder)")
