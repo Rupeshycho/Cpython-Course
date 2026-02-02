c9acb601b44eb3c25d0ddeaf2cfe19791fe1b197
Rupeshycho <crrupesh0@gmail.com>
2026-02-02 23:17:52 +0545

    slicing & operations added

---
+++ day12sliceoperations.py
@@ -0,0 +1,15 @@
+names  ="01234 6789"
+print(names[1::3]) #1 and step value 3 >147
+print(names[0:5]) #01234
+fruit='mango'
+print(len(fruit))
+print("mango is a ", len(fruit), "lettered word")
+print(fruit[:5]) #starts from 0
+print(fruit[0:-2]) #man
+print(fruit[0:len(fruit)-2]) #man
+print(fruit[-1:-2]) # no output
+name="marriageanjuwedsrupesh"
+print(name[-14:len(name)])
+
+marriage=name[-14:len(name)]
+print(marriage.capitalize())
