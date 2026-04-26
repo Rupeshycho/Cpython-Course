83f6a6f4237c8e890de2f93adb317d71f62ae4bd
RupeshY <crrupesh0@gmail.com>
2026-04-26 09:23:45 +0545

    encode & decode added

---
+++ encoding.py
@@ -0,0 +1,18 @@
+# import encoding
+# import decodestring()
+# encoded = "Rupesh".encode("ASCII")
+# print(encoded)
+
+
+
+# #input
+# encod=input("Enter the string: ")
+# print(encod.encode("ASCII"))
+
+
+text="Hello Rupesh"
+encoded = text.encode("UTF-8")
+print("Encode: \n",encoded)
+
+decoded = encoded.decode("UTF-8")
+print("Decode: \n", decoded)
